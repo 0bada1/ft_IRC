d52046e7e4786fa723017bc7686c85593cdb65ba
0bada1 <obadaoutabachi2003@hotmail.com>
2024-02-20 20:08:48 +0400

    First commit code test

---
+++ includes/Server.hpp
@@ -0,0 +1,17 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#incldue <iostream>
+#include <string>
+#incldue <vector>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <fcntl.h>
+#include <cerrno>
+#include <iomanip>
+#include <signal.h>
+#include <stdint.h>
+#include <unistd.h>
+
+# endif // SERVER_HPP
--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+#include "Server.hpp"
+
+int checkInput(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <port number> <password>" << std::endl;
+        return -1;
+    }
+    return 0;
+
+}
+
 int main(int argc, char *argv[])
 {
     if (checkInput(argc, argv) < 0)
