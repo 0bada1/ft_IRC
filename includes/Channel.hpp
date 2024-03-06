cb81cd491969bedaf94130437139ac6bdccf136b
Obada Outabachi <95438648+0bada1@users.noreply.github.com>
2024-03-06 20:09:43 +0400

    Update Channel.hpp

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -1,9 +1,12 @@
 #ifndef CHANNEL_HPP
 #define CHANNEL_HPP
 
-#include "Server.hpp"
-#include "User.hpp"
+#include <iostream> // already included in Server.hpp
+#include <sstream>
 #include <map>
+#include <vector> // already included in Server.hpp
+#include "User.hpp"
+#include "Server.hpp"
 
 // Channel modes
 // #define INVITE_ONLY_CHANNEL 50
@@ -17,10 +20,13 @@
 #define ERR_TOOMANYCHANNELS_M 	" :You have joined too many channels\n" // 405	Sent to a user when they have joined the maximum number of allowed channels and they try to join another channel.
 #define ERR_USERNOTINCHANNEL_M	" :They aren't on that channel\n" // 441		Returned by the server to indicate that the target user of the command is not on the given channel.
 #define ERR_NOTONCHANNEL_M		" :You're not on that channel\n" // 442			Returned by the server to indicate that the target user of the command is not on the given channel.
+#define ERR_USERONCHANNEL_M		" :is already on channel" // 443				Returned when a client tries to invite a user to a channel they are already on.
+#define ERR_NOTREGISTERED_M		" :You have not registered\n" // 451			Returned by the server to indicate that the client MUST be registered before the server will allow it to be parsed in detail.
+#define ERR_NEEDMOREPARAMS_M	" :Not enough parameters\n" // 461				Returned by the server by numerous commands to indicate to the client that it didn't supply enough parameters.
 #define ERR_CHANNELISFULL_M 	" :Channel is full\n" // 471					Sent to a user who is trying to join a channel which is set +l and is already full.
 #define ERR_UNKNOWNMODE_M		" :is unknown mode char to me for " // 472		Used to indicate that a given mode char is unknown.
-#define ERR_INVITEONLYCHAN_M 	" :Channel is invite only\n" // 473				Sent to a user trying to join a channel which is invite-only (+i) without an invitation.
-#define ERR_BANNEDFROMCHAN_M 	" :You are banned from this channel\n" // 474	Sent to a user trying to join a channel where the user is banned.
+#define ERR_INVITEONLYCHAN_M 	" :Cannot join channel (+i)\n" // 473				Sent to a user trying to join a channel which is invite-only (+i) without an invitation.
+#define ERR_BANNEDFROMCHAN_M 	" :Cannot join channel (+b)\n" // 474			Sent to a user trying to join a channel where the user is banned.
 #define ERR_BADCHANNELKEY_M 	" :Cannot join channel (+k)" // 475				Sent to a user trying to join a channel with a key when the channel has been set +k (or mode +k).
 #define ERR_CHANOPRIVSNEEDED_M	" :You're not channel operator" // 482			Any command requiring 'chanop' privileges (such as MODE messages) MUST return this error if the client making the attempt is not a chanop on the specified channel.
 
@@ -31,10 +37,13 @@
 #define ERR_TOOMANYCHANNELS_C 	405	// " :You have joined too many channels\n"	Sent to a user when they have joined the maximum number of allowed channels and they try to join another channel.
 #define ERR_USERNOTINCHANNEL_C	441	// " :They aren't on that channel\n"		Returned by the server to indicate that the target user of the command is not on the given channel.
 #define ERR_NOTONCHANNEL_C		442 // " :You're not on that channel\n"			Returned by the server to indicate that the target user of the command is not on the given channel.
+#define ERR_USERONCHANNEL_C		443 // " :is already on channel"				Returned when a client tries to invite a user to a channel they are already on.
+#define ERR_NOTREGISTERED_C		451	// " :You have not registered\n"			Returned by the server to indicate that the client MUST be registered before the server will allow it to be parsed in detail.
+#define ERR_NEEDMOREPARAMS_C	461 // " :Not enough parameters\n"				Returned by the server by numerous commands to indicate to the client that it didn't supply enough parameters.
 #define ERR_CHANNELISFULL_C 	471	// " :Channel is full\n"					Sent to a user who is trying to join a channel which is set +l and is already full.
 #define ERR_UNKNOWNMODE_C		472	// " :is unknown mode char to me for "		Used to indicate that a given mode char is unknown.
-#define ERR_INVITEONLYCHAN_C 	473	// " :Channel is invite only\n"				Sent to a user trying to join a channel which is invite-only (+i) without an invitation.
-#define ERR_BANNEDFROMCHAN_C 	474	// " :You are banned from this channel\n"	Sent to a user trying to join a channel where the user is banned.
+#define ERR_INVITEONLYCHAN_C 	473	// " :Cannot join channel (+i)\n"			Sent to a user trying to join a channel which is invite-only (+i) without an invitation.
+#define ERR_BANNEDFROMCHAN_C 	474	// " :Cannot join channel (+b)\n"			Sent to a user trying to join a channel where the user is banned.
 #define ERR_BADCHANNELKEY_C 	475	// " :Cannot join channel (+k)"				Sent to a user trying to join a channel with a key when the channel has been set +k (or mode +k).
 #define ERR_CHANOPRIVSNEEDED_C	482	// " :You're not channel operator"			Any command requiring 'chanop' privileges (such as MODE messages) MUST return this error if the client making the attempt is not a chanop on the specified channel.
 
@@ -49,20 +58,27 @@ class Channel
 {
 	public:
 		// CONSTRUCTORS
-		// Channel();
-		Channel(string channel_name, char type);
+		Channel(string channel_name);
 		~Channel();
 
-		// OVERLOADS
+		// OPERATOR OVERLOADS
 		bool operator==(const Channel& other) const;
 
 		// METHODS
 		// int	create_channel(string channel_name);
-		int		set_Channel_Modes(Channel channel, int mode, bool mode_state);
+		// int		set_Channel_Modes(Channel channel, int mode, bool mode_state);
+		int		announce_channel(string message);
 		bool	channel_exists(Channel channel);
 		bool	channel_exists(string channel);
-		int		find_user(vector<User> users, User user); // overload to search by string
-		int		removeUser(string user);
+		int		find_user(vector<User> users, User user); // overload to search by string (DONE)
+		int		find_user(vector<User> users, string user);
+		void	addUser(User user);
+		void	addOperator(User user);
+		void	addInvite(User user);
+		void	addBan(User user);
+		void	removeUser(User user);
+		void	removeOperator(User user);
+		void	removeBan(User user);
 
 		// GETTERS
 		int				get_max_users() const;
@@ -74,6 +90,15 @@ class Channel
 		vector<User>	get_operator_list() const;
 		vector<User>	get_invite_list() const;
 		vector<User>	get_ban_list() const;
+		map<char, bool>	get_mode() const;
+
+		// SETTERS
+		int	set_channel_topic(string topic);
+		int set_channel_password(string password);
+		int set_max_users(int max_users);
+		int set_type(char type);
+		int set_channel_name(string channel_name);
+		int set_mode(char mode, bool mode_state);
 
 	private:
 		unsigned int		max_users_;			// Max users allowed in channel, initially unlimited
@@ -81,7 +106,7 @@ class Channel
 		string				channel_name_;		// Channel name
 		string				channel_topic_;		// Channel topic, initially empty
 		string				password_;			// Password for channel, initially empty
-		map<char, int>		mode_;				// 0/false for off, 1/true for on
+		map<char, bool>		mode_;				// 0/false for off, 1/true for on
 		vector<User>		users_;				// List of users in channel
 		vector<User>		operator_list_;		// List of operators in channel
 		vector<User>		invite_list_;		// List of users invited to channel
