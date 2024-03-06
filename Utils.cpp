fd6821948d5290d31ed2fc2825d9af3226712ba2
Obada Outabachi <95438648+0bada1@users.noreply.github.com>
2024-03-06 20:04:43 +0400

    Update Utils.cpp
    May update again

--- Utils.cpp
+++ Utils.cpp
@@ -1,7 +1,10 @@
 #include "includes/Utils.hpp"
-#include "includes/Users.hpp"
+#include "includes/User.hpp"
 
-int	Utils::binarySearch(std::vector<std::string> users, std::string nickname, unsigned int size)
+using namespace std;
+
+template <typename T>
+int Utils::binarySearch(vector<T> users, T nickname, unsigned int size)
 {
 	int low = 0;
 	int high = size - 1;
@@ -20,19 +23,67 @@ int	Utils::binarySearch(std::vector<std::string> users, std::string nickname, un
 	return -1;
 }
 
-int	Utils::user_exists(std::string nickname)
-{
-	if (binarySearch(Users::nickname_, nickname, Users::userList_.size()) != -1)
-		return 0;
-	return -1;
+/**
+ * @brief Sends an error message to the client as follows\n
+ * <key> ERROR: <message>
+ * 
+ * @param fd 
+ * @param message 
+ * @param key 
+ * @return int 
+ */
+int	Utils::sendErrorMessage(int fd, const string& message, const int key) {
+	string errorMsg = key + " ERROR: " + message;
+	send(fd, errorMsg.c_str(), strlen(errorMsg.c_str()), 0);
+	return 0;
 }
 
-// Mutasem
+User	Utils::find_User(string nickname)
+{
+	for (int i = 0; i < Server::users_.size(); i++)
+	{
+		if (Server::users_[i].getNickname() == nickname)
+			return Server::users_[i];
+	}
+	return NULL;
+}
+
+User	Utils::find_User(User nickname)
+{
+	for (int i = 0; i < Server::users_.size(); i++)
+	{
+		if (Server::users_[i] == nickname)
+			return Server::users_[i];
+	}
+	return NULL;
+}
+
+bool	Utils::username_exists(string username)
+{
+	for (int i = 0; i < Server::users_.size(); i++)
+	{
+		if (Server::users_[i].getUsername() == username)
+			return true;
+	}
+	return false;
+}
+
+bool	Utils::nickname_exists(string nickname)
+{
+	for (int i = 0; i < Server::users_.size(); i++)
+	{
+		if (Server::users_[i].getNickname() == nickname)
+			return true;
+	}
+	return false;
+}
+
+/** @author Mutasem mmajid-m */
 #include "includes/Server.hpp"
 #include "includes/Command.hpp"
 
 User &Utils::find(int fd) {
-	for(std::vector<User>::iterator it = Server::_users.begin(); it != Server::_users.end(); ++it) {
+	for(vector<User>::iterator it = Server::_users.begin(); it != Server::_users.end(); ++it) {
         if (it->_fd == fd) {
 			return *it;
         }
@@ -42,9 +93,9 @@ User &Utils::find(int fd) {
 
 void Utils::signalHandler(int signum) {
 
-    std::cout << RED << "Interrupt signal (" << signum << ") received." << RESET << "\n";
+    cout << RED << "Interrupt signal (" << signum << ") received." << RESET << "\n";
 
-    for(std::vector<int>::iterator it = Server::_fds.begin(); it != Server::_fds.end(); ++it) {
+    for(vector<int>::iterator it = Server::_fds.begin(); it != Server::_fds.end(); ++it) {
             close(*it);
     }
 	shutdown(Server::serverSocket, SHUT_RDWR);
@@ -58,13 +109,13 @@ void Utils::signalHandler(int signum) {
 // void Utils::closeThis(User &user)
 // {
 // 	close(user._fd);
-// 	std::vector<User>::iterator it_u;
-// 	std::vector<User>::iterator it_o;
-// 	std::vector<User>::iterator it_i;
+// 	vector<User>::iterator it_u;
+// 	vector<User>::iterator it_o;
+// 	vector<User>::iterator it_i;
 
-//     Server::_fds.erase(std::find(Server::_fds.begin(), Server::_fds.end(), user._fd));	
-//     Server::_users.erase(std::find(Server::_users.begin(), Server::_users.end(), user));
-// 	for (std::vector<Channel>::iterator it = Server::_channels.begin(); it != Server::_channels.end(); it++)
+//     Server::_fds.erase(find(Server::_fds.begin(), Server::_fds.end(), user._fd));	
+//     Server::_users.erase(find(Server::_users.begin(), Server::_users.end(), user));
+// 	for (vector<Channel>::iterator it = Server::_channels.begin(); it != Server::_channels.end(); it++)
 // 	{
 // 		it_u = it->user_in_chan(Server::sd);
 // 		it_o = it->op_in_chan(Server::sd);
@@ -84,34 +135,34 @@ void Utils::signalHandler(int signum) {
 // }
 
 
-// std::string Utils::to_string(int value) {
-//     std::stringstream ss;
+// string Utils::to_string(int value) {
+//     stringstream ss;
 // 	ss << value;
-// 	std::string s = ss.str();
+// 	string s = ss.str();
 // 	return s;
 // }
-// std::vector<std::string> Utils::split(const std::string str) {
-// 	std::vector<std::string> vector;
-// 	std::istringstream iss(str);
-// 	std::string cmd;
-// 	while (iss >> std::skipws >> cmd)
+// vector<string> Utils::split(const string str) {
+// 	vector<string> vector;
+// 	istringstream iss(str);
+// 	string cmd;
+// 	while (iss >> skipws >> cmd)
 // 		vector.push_back(cmd);
 // 	return vector;
 // }
-// std::string Utils::trim(std::string &str) {
+// string Utils::trim(string &str) {
 //     size_t left = str.find_first_not_of('\n');
 //     size_t right = str.find_last_not_of('\n');
-//     if (left == std::string::npos || right == std::string::npos)
+//     if (left == string::npos || right == string::npos)
 //         return "";
 //     return str.substr(left, right - left + 1);
 // }
 
-// void sendErrorMessage(int fd, const std::string& message, const std::string& key) {
-//     std::string errorMsg = key + " ERROR: " + message;
+// void sendErrorMessage(int fd, const string& message, const string& key) {
+//     string errorMsg = key + " ERROR: " + message;
 //     send(fd, errorMsg.c_str(), strlen(errorMsg.c_str()), 0);
 // }
 
-// void handleJoinCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user) {
+// void handleJoinCommand(const vector<string>& splitmsg, Command& cmd, User* user) {
 //     if (splitmsg.size() == 2) {
 //         cmd.join(splitmsg.at(1), "", *user);
 //     } else if (splitmsg.size() == 3){
@@ -122,7 +173,7 @@ void Utils::signalHandler(int signum) {
 //     }
 // }
 
-// void handleKickCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user) {
+// void handleKickCommand(const vector<string>& splitmsg, Command& cmd, User* user) {
 //     if (splitmsg.size() >= 3) {
 //         cmd.kick(splitmsg.at(1), splitmsg.at(2), splitmsg, *user);
 //     } else {
@@ -130,7 +181,7 @@ void Utils::signalHandler(int signum) {
 //     }
 // }
 
-// void handlePrivMsgCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user) {
+// void handlePrivMsgCommand(const vector<string>& splitmsg, Command& cmd, User* user) {
 // 	if (splitmsg.size() >= 3) {
 // 		cmd.privmsg(splitmsg.at(1), splitmsg, *user); // second argument will be the split message for mutiple words
 // 	} else if (splitmsg.size() == 2) {
@@ -139,20 +190,20 @@ void Utils::signalHandler(int signum) {
 // 	} else if(splitmsg.size() == 1) {
 // 		sendErrorMessage(user->_fd, "PRIVMSG command requires atleast 3 arguments\n", ERR_NOSUCHNICK);
 // 	} else { // if PRIVMSG nickname exist and msg dosent exist
-// 		std::string S = ERR_NEEDMOREPARAMS;
+// 		string S = ERR_NEEDMOREPARAMS;
 // 		S.append(" :Not enough parameters\r\n");
 // 		send(user->_fd, S.c_str(), strlen(S.c_str()), 0);
 // 		return;
 // 	}
 // }
 
-// void handleNoticeCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user) {
+// void handleNoticeCommand(const vector<string>& splitmsg, Command& cmd, User* user) {
 // 	if (splitmsg.size() >= 3) {
 // 		cmd.notice(splitmsg.at(1), splitmsg, *user); // second argument will be the split message for mutiple words
 // 	}
 // }
 
-// void	handleInviteCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user)
+// void	handleInviteCommand(const vector<string>& splitmsg, Command& cmd, User* user)
 // {
 // 	if (splitmsg.size() == 3) {
 // 		cmd.invite(splitmsg.at(1), splitmsg.at(2), *user);
@@ -161,7 +212,7 @@ void Utils::signalHandler(int signum) {
 // 	}
 // }
 
-// void	handleTopicCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user)
+// void	handleTopicCommand(const vector<string>& splitmsg, Command& cmd, User* user)
 // {
 // 	if (splitmsg.size() == 3) {
 // 		cmd.topic(splitmsg.at(1), splitmsg.at(2), *user);
@@ -172,17 +223,17 @@ void Utils::signalHandler(int signum) {
 // 	}
 // }
 
-// void handleWhoisCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user) {
+// void handleWhoisCommand(const vector<string>& splitmsg, Command& cmd, User* user) {
 // 	(void)cmd;
 // 	if (splitmsg.size() == 2) {
-// 		std::string nick = "name : " + splitmsg.at(1) + "\r\n";
+// 		string nick = "name : " + splitmsg.at(1) + "\r\n";
 // 		send(user->_fd, nick.c_str(), strlen(nick.c_str()), 0);
 // 	} else {
 // 		sendErrorMessage(user->_fd, "WHOIS command requires 2 arguments\n", TOO_MANY_ARGS);
 // 	}
 // }
 
-// void handleModeCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user)
+// void handleModeCommand(const vector<string>& splitmsg, Command& cmd, User* user)
 // {
 // 	if (splitmsg.size() == 3){
 // 		cmd.mode(splitmsg.at(1), splitmsg.at(2), *user, "");
@@ -196,7 +247,7 @@ void Utils::signalHandler(int signum) {
 		
 // }
 
-// void handlePartCommand(const std::vector<std::string>& splitmsg, Command& cmd, User* user)
+// void handlePartCommand(const vector<string>& splitmsg, Command& cmd, User* user)
 // {
 // 	if (splitmsg.size() == 2){
 // 		cmd.part(splitmsg.at(1), *user);
@@ -207,7 +258,7 @@ void Utils::signalHandler(int signum) {
 // }
 
 User &Utils::find(int fd) {
-	for(std::vector<User>::iterator it = Server::_users.begin(); it != Server::_users.end(); ++it) {
+	for(vector<User>::iterator it = Server::_users.begin(); it != Server::_users.end(); ++it) {
         if (it->_fd == fd) {
 			return *it;
         }
@@ -217,9 +268,9 @@ User &Utils::find(int fd) {
 
 void Utils::signalHandler(int signum) {
 
-    std::cout << RED << "Interrupt signal (" << signum << ") received." << RESET << "\n";
+    cout << RED << "Interrupt signal (" << signum << ") received." << RESET << "\n";
 
-    for(std::vector<int>::iterator it = Server::_fds.begin(); it != Server::_fds.end(); ++it) {
+    for(vector<int>::iterator it = Server::_fds.begin(); it != Server::_fds.end(); ++it) {
             close(*it);
     }
 	shutdown(Server::serverSocket, SHUT_RDWR);
