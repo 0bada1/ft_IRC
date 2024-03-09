b9d48cf2c0f1fe37c2e35bed385803f90dab8aff
Obada Outabachi <95438648+0bada1@users.noreply.github.com>
2024-03-09 17:00:44 +0400

    Update Commands.hpp

--- includes/Commands.hpp
+++ includes/Commands.hpp
@@ -10,6 +10,7 @@
 #include "User.hpp"
 
 using namespace std;
+
 class Commands
 {
     public:
@@ -20,9 +21,11 @@ class Commands
         // METHODS
         int join(Channel channel, User user, string key);
         int kick(Channel channel, User user, string kicked_user, string reason);
-        int invite(Channel channel, User user, User invited_user);
+        int invite(Channel channel, User user, string invited_user);
         int privmsg(Channel channel, User user, vector<string> message);
         int privmsg(string receiver, User user, vector<string> message);
+        int notice(User user, vector<string> message);
+        int notice(string receiver, User user, vector<string> message);
         int topic(Channel channel, User user, string topic);
         int mode (Channel channel, User user, char mode, char state, string argument);
         int part(Channel channel, User user);
